aade333fb810e8c5482855c11e854ab02cfdcc23
woot <worrawutp@gmail.com>
2024-05-02 16:24:58 +0700

    Ensure pdf files can be link via Vite assets

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+import project1PdfUrl from "./pdffile/Scicreme/Sci-Cream.pdf";
+import project2PdfUrl from "./pdffile/EasyPay/KMITL-Easy-Pay-App.pdf";
+import project3PdfUrl from "./pdffile/Pharmakub/Pharmakub.pdf";
+import project4PdfUrl from "./pdffile/IhavePet/IHavePET.pdf";
 import './src/test.js'
 import './src/style.css'
 import './src/box1.css'
@@ -14,3 +18,12 @@ import './src/box4_left.css'
 import './src/box4_right.css'
 
 console.log("Hi")
+
+const linkProject1 = document.querySelector(".project1 a")
+const linkProject2 = document.querySelector(".project2 a")
+const linkProject3 = document.querySelector(".project3 a")
+const linkProject4 = document.querySelector(".project4 a")
+linkProject1.href = project1PdfUrl
+linkProject2.href = project2PdfUrl
+linkProject3.href = project3PdfUrl
+linkProject4.href = project4PdfUrl
